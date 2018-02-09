34d6efa65ae653601877e883bc297cd085534d63
Evan Winter <ewinter1996@gmail.com>
2018-02-09 13:45:08 -0600

    .

--- js/index.js
+++ js/index.js
@@ -6,10 +6,11 @@ let totalCharCount = 0,
 
 // Array of possible texts on which user will be tested.
 const sampleTexts = [
-	"Glamour cannot exist without personal social envy being a common and widespread emotion. The industrial society which has moved towards democracy and then stopped half way is the ideal society for generating such an emotion. The pursuit of individual happiness has been acknowledged as a universal right. Yet the existing social condition make the individual feel powerless. He lives in the contradiction between what he is and what he would like to be. Either he then becomes fully conscious of the contradiction and its causes, and so joins the political struggle for a full democracy which entails, amongst other things, the overthrow of capitalism; or else he lives, continually subject to an envy which, compounded with his sense of powerlessness, dissolves into recurrent day-dreams.",
-	"We believe that we can change the things around us in accordance with our desires. We believe it because otherwise we can see no favourable outcome. We do not think of the outcome which generally comes to pass and is also favourable: we do not succeed in changing things in accordance with our desires, but gradually our desires change. The situation that we hoped to change because it was intolerable becomes unimportant to us. We have failed to surmount the obstacle, as we were absolutely determined to do, but life has taken us round it, led us beyond it, and then if we turn round to gaze into the distance of the past, we can barely see it, so imperceptible has it become.",
-	"The most important things are the hardest to say. They are the things you get ashamed of, because words diminish them. Words shrink things that seemed limitless when they were in your head to no more than living size when they're brought out. But it's more than that, isn't it? The most important things lie too close to wherever your secret heart is buried, like landmarks to a treasure your enemies would love to steal away. And you may make revelations that cost you dearly only to have people look at you in a funny way, not understanding what you've said at all, or why you thought it was so important that you almost cried while you were saying it. That's the worst, I think. When the secret stays locked within not for want of a teller but for want of an understanding ear.",
-	"If you're going to try, go all the way. Otherwise, don't even start. This could mean losing girlfriends, wives, relatives and maybe even your mind. It could mean not eating for three or four days. It could mean freezing on a park bench. It could mean jail. It could mean derision. It could mean mockery, isolation. Isolation is the gift. All the others are a test of your endurance, of how much you really want to do it. And, you'll do it, despite rejection and the worst odds. And it will be better than anything else you can imagine. If you're going to try, go all the way. There is no other feeling like that. You will be alone with the gods, and the nights will flame with fire. You will ride life straight to perfect laughter. It's the only good fight there is."
+	"Hello my name is evan winter and this is my typing test."
+	// "Glamour cannot exist without personal social envy being a common and widespread emotion. The industrial society which has moved towards democracy and then stopped half way is the ideal society for generating such an emotion. The pursuit of individual happiness has been acknowledged as a universal right. Yet the existing social condition make the individual feel powerless. He lives in the contradiction between what he is and what he would like to be. Either he then becomes fully conscious of the contradiction and its causes, and so joins the political struggle for a full democracy which entails, amongst other things, the overthrow of capitalism; or else he lives, continually subject to an envy which, compounded with his sense of powerlessness, dissolves into recurrent day-dreams.",
+	// "We believe that we can change the things around us in accordance with our desires. We believe it because otherwise we can see no favourable outcome. We do not think of the outcome which generally comes to pass and is also favourable: we do not succeed in changing things in accordance with our desires, but gradually our desires change. The situation that we hoped to change because it was intolerable becomes unimportant to us. We have failed to surmount the obstacle, as we were absolutely determined to do, but life has taken us round it, led us beyond it, and then if we turn round to gaze into the distance of the past, we can barely see it, so imperceptible has it become.",
+	// "The most important things are the hardest to say. They are the things you get ashamed of, because words diminish them. Words shrink things that seemed limitless when they were in your head to no more than living size when they're brought out. But it's more than that, isn't it? The most important things lie too close to wherever your secret heart is buried, like landmarks to a treasure your enemies would love to steal away. And you may make revelations that cost you dearly only to have people look at you in a funny way, not understanding what you've said at all, or why you thought it was so important that you almost cried while you were saying it. That's the worst, I think. When the secret stays locked within not for want of a teller but for want of an understanding ear.",
+	// "If you're going to try, go all the way. Otherwise, don't even start. This could mean losing girlfriends, wives, relatives and maybe even your mind. It could mean not eating for three or four days. It could mean freezing on a park bench. It could mean jail. It could mean derision. It could mean mockery, isolation. Isolation is the gift. All the others are a test of your endurance, of how much you really want to do it. And, you'll do it, despite rejection and the worst odds. And it will be better than anything else you can imagine. If you're going to try, go all the way. There is no other feeling like that. You will be alone with the gods, and the nights will flame with fire. You will ride life straight to perfect laughter. It's the only good fight there is."
 ];
 
 const timer = document.getElementById('timer'),
@@ -75,18 +76,24 @@ function disableInput() {
 function getUserInput(currentIndex, inputField, splitText) {
 	const highlighted = document.getElementById('current');
 	inputField.addEventListener('keydown', function(event) {
-		// Every time user presses spacebar...
+			// Every time user presses spacebar...
 		if (event.which === 32) {
-			let thisWord = inputField.value.trim();
-			if (thisWord !== '') {	// Only execute they typed something
-				// Update total character and word counts.
-				totalCharCount += thisWord.length;
-				totalWordCount++;
-				// Update the user's total score.
-				totalPenalties += getWordPenalty(currentIndex, splitText, thisWord);
-				// Highlight the next word to be typed, and shift words as needed.
-				updateSampleText(++currentIndex, splitText);
-				inputField.value = '';	// Clear the input field
+			// If not at the end of the template...
+			if (currentIndex+1 < splitText.length) {
+				let thisWord = inputField.value.trim();
+				if (thisWord !== '') {	// Only execute if they typed something
+					// Update total character and word counts.
+					totalCharCount += thisWord.length;
+					totalWordCount++;
+					// Update the user's total score.
+					totalPenalties += getWordPenalty(currentIndex, splitText, thisWord);
+					// Highlight the next word to be typed, and shift words as needed.
+					updateSampleText(++currentIndex, splitText);
+					inputField.value = '';	// Clear the input field
+				}
+			} else {
+				inputField.value = '';
+				template.innerHTML = 'Congrats! You reached the end of the test.'
 			}
 		}
 	});
